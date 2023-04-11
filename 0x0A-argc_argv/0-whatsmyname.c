ac9c31ce097249dba0c65017c39c907ba6eae4e6
MIchael <miteldream@gmail.com>
2023-04-11 05:29:34 +0100

    My New 0x0A. C - argc, argv Project

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - entry point of the program
+ * main - Prints the name of the program
+ * @argc: The number of arguments passed
+ * @argv: An array containing the arguments passed to the program
  *
  * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
+    (void)argc; /* Suppress unused parameter warning */
+
     printf("%s\n", argv[0]);
+
     return 0;
 }
